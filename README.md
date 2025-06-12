bad80254994d02433ed9f9d189c5bb0b568c5e79
chenxiang <704991100@qq.com>
2025-06-13 00:33:46 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# tsun-smart-ui
+
+UI Library
+
+## Installation
+
+```sh
+npm install tsun-smart-ui
+```
+
+## Usage
+
+
+```js
+import { multiply } from 'tsun-smart-ui';
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
+
+---
+
+Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+/*
+ * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @Date: 2025-06-12 23:56:59
+ * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+ * @LastEditTime: 2025-06-13 00:31:48
+ * @FilePath: \TSUN_SMART_UI\src\components\Button\index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+const Button =()=>{
+    return <></>
+}
+export default Button
