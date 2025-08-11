623e9a23961e7e1d80de36703be59a33436aa0a2
chenxiang <704991100@qq.com>
2025-08-11 21:24:11 +0800

    fix: 修复倒计时显示问题

--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -36,7 +36,7 @@ const parseMs = (milliseconds: number) => {
     days: Math.floor(milliseconds / 86400000),
     hours: Math.floor(milliseconds / 3600000) % 24,
     minutes: Math.floor(milliseconds / 60000) % 60,
-    seconds: Math.floor(milliseconds / 1000) % 60,
+    seconds: Math.floor(milliseconds / 1000),
     milliseconds: Math.floor(milliseconds) % 1000,
   };
 };
